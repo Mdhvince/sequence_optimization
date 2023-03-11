42232368df9e8a907f9b06c6bac3768a9bdd0897
mdhvince <vinceslas.medhy@gmail.com>
2023-03-11 10:05:01 +0100

    share weights

--- src/vpg.py
+++ src/vpg.py
@@ -1,11 +1,16 @@
+import random
 import warnings
 from itertools import count
+from collections import deque
 
 import torch
 import numpy as np
+import pandas as pd
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 
-from fc import PolicyVPG, ValueVPG
+from environment import EnvSeqV1
+from networks import PolicyVPG, ValueVPG
 
 warnings.filterwarnings('ignore')
 
@@ -16,8 +21,8 @@ class Agent:
         self.gamma = .99
         lr_p = 0.0005
         lr_v = 0.0007
-        hidden_dims_p = (128, 64)
-        hidden_dims_v = (256, 128)
+        hidden_dims_p = (512, 256, 128, 32)
+        hidden_dims_v = (512, 256, 128, 64, 32)
         self.entropy_loss_weight = 0.001
         self.C = state_shape[0]
 
@@ -27,7 +32,7 @@ class Agent:
         self.p_max_grad = 1
 
         self.value_model = ValueVPG(self.device, state_shape, hidden_dims=hidden_dims_v).to(self.device)
-        self.v_optimizer = optim.RMSprop(self.value_model.parameters(), lr=lr_v)
+        self.v_optimizer = optim.Adam(self.value_model.parameters(), lr=lr_v)
         self.v_max_grad = float("inf")
 
         self.logpas = []
@@ -114,4 +119,58 @@ class Agent:
 
 
 if __name__ == "__main__":
-    pass
+    writer = SummaryWriter("runs/vpg")
+
+    seed = 42
+    model_path = "agent_vpg.pt"
+    n_episodes = 15000
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    wc = pd.read_csv("notebooks/WC.csv", sep=";").iloc[:20, :]
+    wc = wc.sample(frac=1, random_state=seed)
+
+    print(len(wc))
+
+    positions = wc.columns
+    takt_time = np.ones(len(positions)) * 59
+    buffer_percent = np.array([1.45, 1.60, 1.25, 1.50, 1.25, 1.25, 1.25, 1.50, 1.25, 1.25, 1.25, 1.25, 1.25])
+
+    env = EnvSeqV1(wc, takt_time, buffer_percent)
+    nA = env.action_space
+    state_shape = env.reset().shape
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    agent = Agent(state_shape, nA, device)
+
+    last_100_score = deque(maxlen=100)
+    last_100_stoppage_duration = deque(maxlen=100)
+    last_100_stoppage_pp = deque(maxlen=100)
+
+    for i_episode in range(1, n_episodes + 1):
+        shuffle = True
+        state, is_terminal = env.reset(shuffle), False
+
+        agent.reset_metrics()
+
+        for t_step in count():
+            next_state, is_terminal = agent.interact_with_environment(state, env, t_step)
+            state = next_state
+            if is_terminal: break
+
+        next_value = 0 if is_terminal else agent.value_model(state).detach().item()
+        agent.rewards.append(next_value)
+        agent.learn()
+
+        total_rewards, stoppage_pp, sequence = agent.evaluate_one_episode(env, shuffle)
+        last_100_stoppage_pp.append(stoppage_pp / 60)
+
+        mean_stoppage_per_position = np.mean(np.array(last_100_stoppage_pp), axis=0)
+        writer.add_scalars(
+            f"Mean down time per position (minutes)",
+            dict(zip(positions, mean_stoppage_per_position)), i_episode
+        )
+
+    writer.close()
