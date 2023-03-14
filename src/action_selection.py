fix the computation of argmax_q_next

src/action_selection.py
@@ -19,8 +19,8 @@ class EGreedyExpStrategy:
         self.t += 1
         return self.epsilon
 
-    def select_action(self, model, state, nA, illegal_actions=None):
-        action = e_greedy_action_selection(model, state, self.epsilon, nA, illegal_actions)
+    def select_action(self, model, state, nA):
+        action = e_greedy_action_selection(model, state, self.epsilon, nA)
         self._epsilon_update()
         return action
 
@@ -34,8 +34,9 @@ class GreedyStrategy:
         with torch.no_grad():
             q_values = model(state).cpu().detach().data.numpy().squeeze()
 
-        top2actions = q_values.argsort()[-2:][::-1]
-        return top2actions
+        # top2actions = q_values.argsort()[-2:][::-1]
+        action = np.argmax(q_values)
+        return action
 
 
 def e_greedy_action_selection(model, state, epsilon, nA, illegal_actions=None):
@@ -44,20 +45,14 @@ def e_greedy_action_selection(model, state, epsilon, nA, illegal_actions=None):
         with torch.no_grad():
             q_values = model(state).cpu().detach().data.numpy().squeeze()
         model.train()
-        # action = np.argmax(q_values)
+        action = np.argmax(q_values)
 
-        # Down the value of illegal actions
-        # if illegal_actions is not None:
-        #     while action in illegal_actions:
-        #         q_values[action] = -INF
-        #         action = np.argmax(q_values)
-
-        top2actions = q_values.argsort()[-2:][::-1]
+        # top2actions = q_values.argsort()[-2:][::-1]
 
     else:
-        # action = np.random.randint(nA)
-        top2actions = np.random.randint(0, nA, size=2)
-    return top2actions
+        action = np.random.randint(nA)
+        # top2actions = np.random.randint(0, nA, size=2)
+    return action
 
 
 if __name__ == "__main__":

src/dqn.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 
 from src.networks import DuelingDQN
-from src.environment import EnvSeqV1
+from src.environment import EnvSeqV1, EnvSeqV2
 from src.action_selection import EGreedyExpStrategy, GreedyStrategy
 from src.replay_buffer import ReplayBuffer
 
@@ -30,6 +30,7 @@ class Agent:
         self.seed = seed
         self.batch_size = 256
         self.nA = nA
+        # self.nA_step = nA_step
         lr = .001
         self.gamma = .995
         self.device = device
@@ -59,19 +60,19 @@ class Agent:
 
     def sample_and_learn(self):
         states, actions, rewards, next_states, dones = self.memory.sample(self.device)
-        states = states.reshape(self.batch_size, self.C, self.nA, -1)
-        next_states = next_states.reshape(self.batch_size, self.C, self.nA, -1)
+        states = states.reshape(self.batch_size, self.C, states.shape[1], -1)
+        next_states = next_states.reshape(self.batch_size, self.C, next_states.shape[1], -1)
 
         states = states.squeeze(1)
         next_states = next_states.squeeze(1)
 
         # Action that have the highest value: Index of action ==> FROM THE BEHAVIOR POLICY
-        # argmax_q_next = self.behavior_policy(next_states).detach().argmax(dim=1).unsqueeze(-1)
-        _, argmax_q_next = torch.topk(self.behavior_policy(next_states).detach(), k=2, dim=1)
+        argmax_q_next = self.behavior_policy(next_states).detach().argmax(dim=1).unsqueeze(-1)
+        # _, argmax_q_next = torch.topk(self.behavior_policy(next_states).detach(), k=self.nA_step, dim=1)
+
         # Action-values of "best" actions  ==> FROM THE TARGET POLICY
         Q_targets_next = self.target_policy(next_states).gather(1, argmax_q_next)
         Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
-
 
         actions = actions.to(torch.int64)
         Q_expected = self.behavior_policy(states).gather(1, actions)
@@ -96,7 +97,6 @@ class Agent:
 
             total_rewards += r
             stoppage_pp += env.stoppage_per_position
-            seq.append(a)
 
             if d: break
 
@@ -130,6 +130,7 @@ class Agent:
                 t.data.copy_(b.data)
 
 
+
 if __name__ == "__main__":
     writer = SummaryWriter("../runs/dqns")
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -144,13 +145,13 @@ if __name__ == "__main__":
     random.seed(SEED)
 
     wc = pd.read_csv("../notebooks/WC.csv", sep=";").sample(frac=1, random_state=SEED)
-    wc = wc.iloc[:20, :]
+    wc = wc.iloc[:50, :]
 
     positions = wc.columns
     takt_time = np.ones(len(positions)) * 59
     buffer_percent = np.array([1.45, 1.60, 1.25, 1.50, 1.25, 1.25, 1.25, 1.50, 1.25, 1.25, 1.25, 1.25, 1.25])
 
-    env = EnvSeqV1(wc, takt_time, buffer_percent)
+    env = EnvSeqV2(wc, takt_time, buffer_percent)
     nA = env.action_space
     s = env.reset()
     state_shape = s.shape
@@ -167,7 +168,7 @@ if __name__ == "__main__":
 
     running_mean_100 = -np.inf
 
-    for i_episode in range(1, N_EPISODES + 1):
+    for e in range(1, N_EPISODES + 1):
         shuffle = False
         state, is_terminal = env.reset(shuffle), False
 
@@ -181,15 +182,14 @@ if __name__ == "__main__":
                 agent.sync_weights(use_polyak_averaging=True)
             if done: break
 
-
         reward_episode, stoppage_pp, sequence = agent.evaluate_one_episode(env, shuffle)
         last_100_reward.append(reward_episode)
         last_100_stoppage_pp.append(stoppage_pp / 60)
 
-        mean_stoppage_per_position = np.mean(np.array(last_100_stoppage_pp), axis=0)
-        writer.add_scalars(
-            f"Mean down time per position (minutes)", dict(zip(positions, mean_stoppage_per_position)), i_episode
-        )
-        writer.add_scalar("Mean reward", np.mean(last_100_reward), i_episode)
+        if e % 100 == 0:
+            mean_stoppage_per_position = dict(zip(positions, np.mean(np.array(last_100_stoppage_pp), axis=0)))
+            mean_rewards = np.mean(last_100_reward)
+            writer.add_scalars("Mean down time per position in minutes", mean_stoppage_per_position, e)
+            writer.add_scalar("Mean reward", mean_rewards, e)
 
     writer.close()

src/environment.py
@@ -5,6 +5,123 @@ import numpy as np
 warnings.filterwarnings('ignore')
 
 
+class EnvSeqV2:
+    def __init__(self, workcontent, takt_time, buffer_percent):
+        """
+        :param workcontent: Pandas dataframe where columns are positions dans values represent the workcontent
+        :param takt_time: 1D array representing the time required per position
+        :param buffer_percent: 1D array representing the buffer length of each position
+        """
+        self.wc = workcontent
+        self.N_POSITIONS = len(workcontent.columns)
+        self.N_SEATS = len(workcontent)
+        self.TAKT_TIME_PP = takt_time
+        self.BUFFER_LENGTH_PP_PERCENT = buffer_percent
+        assert len(self.BUFFER_LENGTH_PP_PERCENT) == self.N_POSITIONS
+
+        self.MAX_DELAY_PP = self.TAKT_TIME_PP * self.BUFFER_LENGTH_PP_PERCENT - self.TAKT_TIME_PP
+
+        self.seat_sequence = None
+        self.delay_matrix = None
+        self.stoppage_matrix = None
+        self.stoppage_per_position = None
+        self.delay_per_position = None
+
+    def reset(self, shuffle=False):
+        self.seat_sequence = np.copy(self.wc.values)
+        if shuffle:
+            np.random.shuffle(self.seat_sequence)
+
+        self.delay_matrix = np.zeros((self.N_SEATS, self.N_POSITIONS))
+        self.stoppage_matrix = np.zeros((self.N_SEATS, self.N_POSITIONS))
+        self.stoppage_per_position = np.zeros(self.N_POSITIONS)
+        self.delay_per_position = np.zeros(self.N_POSITIONS)
+
+        state = self.build_state()
+        return state
+
+    def step(self, action, t_step):
+        is_terminal = t_step == self.N_SEATS - 1
+
+        row_id, direction = self.get_movement_from_action(action)
+        self.update_sequence(row_id, direction)
+        self.update_delay()
+        next_state = self.build_state()
+        reward = self.reward()
+
+        return next_state, reward, is_terminal
+
+    def get_movement_from_action(self, action):
+        if action < self.N_SEATS:
+            direction = -1  # UP
+        elif self.N_SEATS <= action < 2 * self.N_SEATS:
+            direction = 1  # DOWN
+            action = action - self.N_SEATS
+        else:
+            direction = 0  # HOLD
+            action = action - 2 * self.N_SEATS
+
+        return action, direction
+
+    def update_sequence(self, row_id, direction):
+        """
+        :param row_id: Row id to be moved
+        :param direction: -1, 1, 0 for (up, down, hold)
+        """
+        destination_id = (row_id + direction) % len(self.seat_sequence)
+        row = self.seat_sequence[row_id]
+        self.seat_sequence = np.delete(self.seat_sequence, row_id, axis=0)
+        self.seat_sequence = np.insert(self.seat_sequence, destination_id, row, axis=0)
+
+    def update_delay(self):
+        """
+        Update the delay and stoppage matrices (for the new state) and the total stoppage (for the reward)
+        """
+        delay_func = lambda x: 0 if x < 0 else x
+        stoppage_func = lambda delay, max_delay: 0 if delay < max_delay else delay - max_delay
+
+        self.delay_matrix = np.zeros((self.N_SEATS, self.N_POSITIONS))
+        self.delay_per_position = np.zeros(self.N_POSITIONS)
+        self.stoppage_matrix = np.zeros((self.N_SEATS, self.N_POSITIONS))
+        self.stoppage_per_position = np.zeros(self.N_POSITIONS)
+
+        for n, row in enumerate(self.seat_sequence):
+            delay_tmp = row - self.TAKT_TIME_PP + self.delay_per_position
+            self.delay_per_position = np.array([delay_func(i) for i in delay_tmp])
+
+            # for the new state
+            self.delay_matrix[n] = np.copy(self.delay_per_position)
+            stoppage = np.array([stoppage_func(i, self.MAX_DELAY_PP[n]) for n, i in enumerate(self.delay_per_position)])
+            self.stoppage_matrix[n] = np.copy(stoppage)
+
+            # for the reward
+            self.stoppage_per_position += np.copy(stoppage)
+
+    def reward(self):
+        sequence_stoppage = np.sum(self.stoppage_per_position)
+        return -sequence_stoppage + 1
+
+    def build_state(self):
+        sequence = np.copy(self.seat_sequence)
+        delay = np.copy(self.delay_matrix)
+
+        relative_sequence = sequence / np.max(sequence)
+        relative_delay = delay / self.MAX_DELAY_PP.reshape(1, -1)  # how close the delay is to become a stoppage
+        # stoppage = np.copy(self.stoppage_matrix)
+
+        state = np.stack((relative_sequence, relative_delay), axis=0)
+        return state
+
+    @property
+    def observation_space(self):
+        state = self.reset()
+        return len(state)
+
+    @property
+    def action_space(self):
+        return self.N_SEATS + 2 * self.N_SEATS
+
+
 class EnvSeqV1:
     def __init__(self, workcontent, takt_time, buffer_percent):
         """
@@ -112,7 +229,11 @@ class EnvSeqV1:
 
     @property
     def action_space(self):
-        return self.N_SEATS
+        """
+        :return: number of action id (N) - the agent will be able to select action ranging from 0 to N
+        :return: number of action the agent have to take per steps
+        """
+        return self.N_SEATS, 2
 
 
 if __name__ == "__main__":